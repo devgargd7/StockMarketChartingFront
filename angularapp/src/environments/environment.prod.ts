9f0fc58575533b6537234fc010ec9e65037b1fd2
Dev Garg <devgargd7@gmail.com>
2021-06-22 16:49:53 +0530

    initial commit

--- angularapp/src/environments/environment.prod.ts
+++ angularapp/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  UserApiUrl: 'https://shrouded-waters-11504.herokuapp.com' ,
+  importDataApiUrl: 'https://shrouded-waters-11504.herokuapp.com',
+CompanyServiceApiUrl: 'https://shrouded-waters-11504.herokuapp.com',
+StockExchangeServiceApiUrl: 'https://shrouded-waters-11504.herokuapp.com',
+SectorServiceApiUrl: 'https://shrouded-waters-11504.herokuapp.com',
 };
