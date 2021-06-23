b5dd8187d4935f7867c4fa037312b2d722751f24
Dev Garg <devgargd7@gmail.com>
2021-06-23 09:25:33 +0530

    initial commit

--- angularapp/server.js
+++ angularapp/server.js
@@ -4,11 +4,11 @@ const express = require('express');
     const app = express();
 
     // Serve only the static files form the dist directory
-    app.use(express.static(__dirname + '/dist/angularapp-app'));
+    app.use(express.static(__dirname + '/dist/angularapp'));
 
     app.get('/*', function(req,res) {
         
-    res.sendFile(path.join(__dirname+'/dist/angularapp-app/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/angularapp/index.html'));
     });
 
     // Start the app by listening on the default Heroku port
